9f5ad9827c65ecd39fd790b27e3be9a19178fd24
xchmel33 <62353125+xchmel33@users.noreply.github.com>
2020-12-10 00:40:33 +0100

    Add files via upload

--- prec_analysis.c
+++ prec_analysis.c
@@ -1,3 +1,10 @@
+/**
+ * Implementation of imperative language IFJ2020 compiler
+ * @file prec_analysis.c
+ * @author Lukáš Chmelo (xchmel33)
+ * @brief expression parser implementation
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
@@ -12,13 +19,15 @@
 #include "dstring.h"
 #include "code_generator.h"
 
-#define T_S (7)
-#define EXP_OK (0)
+#define T_S (7) //table size
+#define EXP_OK (0) //return if ok
 
+//global variables
 Psa_stack* ActiveStack;
 Token* EMPTY_TOKEN;
 HashTable* table;
 
+//prec table implemented as 2 dimensional array
 const char prec_table[T_S][T_S] = {
         //      *			  +-   */  <!=   (	   )     i	  $ *//
         /*  	+-	    */ { '>', '<', '>', '<',  '>',  '<', '>'},
@@ -30,6 +39,7 @@ const char prec_table[T_S][T_S] = {
         /*	    $	    */ { '<', '<', '<', '<',  ' ',  '<', ' '},
 };
 void ActivateResources(HashTable* ptable) {
+
     ActiveStack = s_init();
     EMPTY_TOKEN = initToken();
     strAddChar(EMPTY_TOKEN->attribute.string, '$');
@@ -43,42 +53,43 @@ void FreeResources() {
 
 int getIndex(Token* T) {
 
-    //pozicia v tabulke
+    //table possition
     switch (T->type) {
         case TT_ADD:
-        case TT_SUB:
-            return 0;
+        case TT_SUB: 
+            return 0; //+-
         case TT_MUL:
         case TT_DIV:
-            return 1;
+            return 1; //*/
         case TT_LESS_THAN:
         case TT_MORE_THAN:
         case TT_EQUAL:
         case TT_MORE_OR_EQUAL:
         case TT_LESS_OR_EQUAL:
         case TT_NOT_EQUAL:
-            return 2;
+            return 2; // <...!=
         case(TT_L_BRACKET):
-            return 3;
+            return 3; //(
         case(TT_R_BRACKET):
-            return 4;
+            return 4; //)
         case TT_IDENTIFIER:
         case TT_INTEGER:
         case TT_DECIMAL:
         case TT_STRING:
-            return 5;
+            return 5; // i
         case TT_EMPTY:
         case TT_SEMICOLON:
         case TT_COMMA:
         case TT_EOL:
-            return 6;
+            return 6; //$
         default:
-            return -1;
+            return -1; //error
     }
 }
 
 Token* getSymbol(Token* A, Token* B) {
 
+    //get prec table symbol 
     int x, y;
     x = getIndex(A);
     y = getIndex(B);
@@ -87,14 +98,15 @@ Token* getSymbol(Token* A, Token* B) {
     }
     Token* sym = initToken();
     sym->type = TT_TABLESYM;
-    strAddChar(sym->attribute.string, prec_table[x][y]);
+    strAddChar(sym->attribute.string, prec_table[x][y]); 
     return sym;
 }
 
-Token* getNextToken(Token* actual_token, Token* next_token) {
+Token* getNextToken(Token* actual_token, Token* next_token, int *error_code) {
 
+    //decide whenever get the next token or use current
     if (actual_token->type == next_token->type) {
-        GetToken(next_token);
+        *error_code = GetToken(next_token);
         return next_token;
     }
     else {
@@ -104,6 +116,7 @@ Token* getNextToken(Token* actual_token, Token* next_token) {
 
 Token* topTerm(Psa_stack* stack) {
 
+    //cycle while terminal nearest to stack top is found
     stack->active = (struct Stack_item*) stack->top;
     while (stack->active != NULL) {
         if (stack->active->E.type >= TT_ADD && stack->active->E.type <= TT_EMPTY) {
@@ -115,7 +128,8 @@ Token* topTerm(Psa_stack* stack) {
 }
 Datatype getType(Token *token) {
 
-
+    /*token type will be lost by TT_EXPRESION, 
+    so it has to be restored by initialized attribute*/
     if (token->attribute.integer != UNDEFINED_TOKEN_ATTRIBUTE) {
         return INT;
     }
@@ -132,19 +146,24 @@ Datatype getType(Token *token) {
 
 bool cg_stack_p(Token* token) {
 
-    if (token->type == TT_STRING) {
+    Datatype t = getType(token);
+    if (t== STRING) {
         cg_stack_push_string(token->attribute.string->str);
+        return true;
     }
-    else if (token->type == TT_INTEGER) {
+    else if (t == INT) {
         cg_stack_push_int(token->attribute.integer);
+        return true;
     }
-    else if (token->type == TT_IDENTIFIER) {
+    else if (t == IDENTIFIER) {
         cg_stack_push_identifier(token->attribute.string->str,table);
+        return true;
     }
-    else if (token->type == TT_DECIMAL) {
+    else if (t == STRING) {
         cg_stack_push_double(token->attribute.decimal);
+        return true;
     }
-    return true;
+    return false;
 }
 
 int checkTypes(Token* operand1,Token* operand2) {
@@ -305,18 +324,10 @@ Token* checkRule(Psa_stack* Rulestack,int *error_code) { //codegen required
     Token *operand1 = s_pop(Rulestack);
     Token *operator = initToken();
     Token *operand2 = initToken();
-    TableItem* ID;
+    TableItem* ID; //for function or variable search
 
-    // E -> E+E
-    // E -> E-E
-    // E -> E*E
-    // E -> E/E
-    // E -> E<E
-    // E -> E>E
-    // E -> E==E
-    // E -> E>=E
-    // E -> E<=E
-    // E -> E!=E
+    
+    // E -> E + ... != E --> all operation rules implementation
     if (operand1->type == TT_EXPRESSION) {
         operator = s_pop(Rulestack);
         operand2 = s_pop(Rulestack);
@@ -334,49 +345,66 @@ Token* checkRule(Psa_stack* Rulestack,int *error_code) { //codegen required
             return EMPTY_TOKEN;
         }
 
+            //codegen operations
+        //push to stack
+        cg_stack_p(operand2);
+        cg_stack_p(operand1);
+
         //string concatanation
-        //if(getType())
-        
+        if (getType(operand1) == STRING && operator->type == TT_ADD && getType(operand2) == STRING) {
+            cg_concat();
+        }
         //all other operations
+        cg_other_operations(operator->type);
 
         //return token with operation datatype
         *error_code = EXP_OK;
         return operand1;
     }
-        // E -> i
+
+    // E -> i --> identifier
     else if (operand1->type == TT_IDENTIFIER){
         ID = htSearch(table, operand1->attribute.string->str);
         if (ID == NULL) {
+            //variable or function not found
             *error_code = ERR_DEF;
             return EMPTY_TOKEN;
         }
         else {
             if (ID->data.var) {
+                //change type to avoid being returned by getTerm function
                 operand1->type = TT_EXPRESSION;
                 *error_code = EXP_OK;
                 return operand1;
             }
             else {
-                //function call
+                //function call & return type stored inside token
                 return funcCall(ID, error_code);
             }
         }
 
     }
+
+    // E -> i --> integer, float, string
     else if (operand1->type >= TT_INTEGER && operand1->type <= TT_STRING) {
         operand1->type = TT_EXPRESSION;
         *error_code = EXP_OK;
         return operand1;
     }
 
-        // E -> (E)
+    // E -> (E)
     else if (operand1->type == TT_L_BRACKET) {
+        
         operand1 = s_pop(Rulestack);
+
+        //return only if previus rule is applied 
         if (s_pop(Rulestack)->type == TT_R_BRACKET && operand1->type == TT_EXPRESSION) {
             *error_code = EXP_OK;
             return operand1;
         }
     }
+
+    //no rule found
     *error_code = ERR_INTERNAL;
     return EMPTY_TOKEN;
 }
@@ -384,66 +412,79 @@ Token* checkRule(Psa_stack* Rulestack,int *error_code) { //codegen required
 
 int expression(Token* prev_token, Token* act_token) {
 
-    Token* A, * B, * Y, *S;
-    Psa_stack* RuleStack;
-    B = prev_token;
-    int pom = 0;
+    Token* A, * B, * Y, * S; //complementary tokens
+    Psa_stack* RuleStack; //temporary stack used to apply operation rules to tokens
+    int error = 0; 
 
+    //push $ to stack bottom
     if (s_push(ActiveStack, EMPTY_TOKEN) != ERR_OK) {
         return ERR_INTERNAL;
     }
 
+    B = prev_token;
     do {
-        A = topTerm(ActiveStack);
-        S = getSymbol(A, B);
+        A = topTerm(ActiveStack); //terminal
+        S = getSymbol(A, B); //prec table symbol
         if (S == NULL) {
             return ERR_PARSER;
         }
         if (B->type != TT_EMPTY || A->type != TT_EMPTY) {
             switch (S->attribute.string->str[0])
             {
-                case '=':
+            case '=': //push only token
+                s_push(ActiveStack, B);
+                B = getNextToken(B, act_token, &error);
+                break;
+            case '<': //push token & symbol
+                if (ActiveStack->top->E.type == TT_EXPRESSION) {
+                    prev_token = s_pop(ActiveStack);
+                    s_push(ActiveStack, S);
+                    s_push(ActiveStack, prev_token);
                     s_push(ActiveStack, B);
-                    B = getNextToken(B, act_token);
-                    break;
-                case '<':
-                    if (ActiveStack->top->E.type == TT_EXPRESSION) {
-                        prev_token = s_pop(ActiveStack);
-                        s_push(ActiveStack, S);
-                        s_push(ActiveStack, prev_token);
-                        s_push(ActiveStack, B);
-                    }
-                    else {
-                        s_push(ActiveStack, S);
-                        s_push(ActiveStack, B);
-                    }
-                    B = getNextToken(B, act_token);
-                    break;
-                case '>':
-                    RuleStack = s_init();
+                }
+                else {
+                    s_push(ActiveStack, S);
+                    s_push(ActiveStack, B);
+                }
+                B = getNextToken(B, act_token, &error);
+                break;
+            case '>': //push tokens to rulestack
+                RuleStack = s_init();
+                Y = s_pop(ActiveStack);
+                while (Y->type != TT_TABLESYM) {
+                    s_push(RuleStack, Y);
                     Y = s_pop(ActiveStack);
-                    while (Y->type != TT_TABLESYM) {
-                        s_push(RuleStack, Y);
-                        Y = s_pop(ActiveStack);
-                    }
-                    //s_push(RuleStack, Y);
-                    s_print(RuleStack, "RuleStack");
-                    s_push(ActiveStack, checkRule(RuleStack, &pom));
-                    if (ActiveStack->top->E.type == TT_EMPTY) {
-                        return pom;
-                    }
-                    s_destroy(RuleStack);
-                    break;
-                case ' ':
-                    return ERR_PARSER;
+                }
+                //push result type stored in token to main stack 
+                s_push(ActiveStack, checkRule(RuleStack, &error)); 
+                if (ActiveStack->top->E.type == TT_EMPTY) {
+                    return error;
+                }
+                s_destroy(RuleStack);
+                break;
+            case ' ':
+                return ERR_PARSER;
+            }
+            if (error != 0) {
+                return error;
             }
         }
-        s_print(ActiveStack, "ActiveStack");
-        if (B->type == TT_EOL || B->type == TT_EOF) {
+
+        //end of expression
+        if (B->type == TT_EOL || B->type == TT_EOF){
+            B = EMPTY_TOKEN;
+        }
+
+        //end of expression when inside statement
+        else if (B->type == TT_BLOCK_BEGIN) {
+            s_push(ActiveStack, B);
             B = EMPTY_TOKEN;
         }
     } while (B->type != TT_EMPTY || A->type != TT_EMPTY);
-    act_token = s_pop(ActiveStack);
+
+    //store result type inside active token
+    act_token = s_pop(ActiveStack); 
+    
     FreeResources();
-    return pom;
+    return error; 
 }
--- prec_analysis.h
+++ prec_analysis.h
@@ -1,3 +1,11 @@
+/**
+ * Implementation of imperative language IFJ2020 compiler
+ * @file prec_analysis.h
+ * @author Lukáš Chmelo (xchmel33)
+ * @brief expression parser header implementation
+ */
+
+
 #ifndef IFJ_FINAL_PREC_ANALYSIS_H
 #define IFJ_FINAL_PREC_ANALYSIS_H
 
@@ -5,16 +13,103 @@
 #include "psa_stack.h"
 #include "symtable.h"
 
+ /**
+  * @brief Function for global variables initialization
+  *
+  * @param ptable, symtable to be initialized
+  *
+  */
 void ActivateResources(HashTable* ptable);
+
+/**
+ * @brief Function for destroing stack
+ */
 void FreeResources();
+
+/**
+ * @brief Function for returning the index of terminal from prec table
+ *
+ * @param T, terminal
+ *
+ * @return integer, index of terminal
+ */
 int getIndex(Token* T);
+
+/**
+ * @brief Function for returning the symbol from prec table
+ *
+ * @param A, top terminal
+ * @param B, current token
+ *
+ * @return token, symbol from prec table
+ */
 Token* getSymbol(Token* A, Token* B);
-Token* getNextToken(Token* prev_token, Token* actual_token);
+
+/**
+ * @brief Function for returning the required token
+ *
+ * @param actual_token, current token
+ * @param next token 
+ *
+ * @return token
+ */
+Token* getNextToken(Token* actual_token, Token* next_token, int* error_code);
+
+/**
+ * @brief Function for returning terminal nearest to stack top
+ *
+ * @param stack
+ *
+ * @return token, which stores the terminal
+ */
 Token* topTerm(Psa_stack* stack);
+
+/**
+ * @brief Function for comparing types of operands
+ *
+ * @param operand1
+ * @param operand2
+ *
+ * @return 0, in case of error, number of error
+ */
 int checkTypes(Token* operand1, Token* operand2);
+
+/**
+ * @brief Function for evaluating type of result from operation as well as applying rules
+ *
+ * @param Rulestack, stores tokens for operations
+ * @param result_type, type of result from operation
+ *
+ * @return 0, in case of error, number of error
+ */
 int checkRule(Psa_stack* Rulestack, Token *result_type);
+
+/**
+ * @brief Function for analyzing lost datatypes
+ *
+ * @param token, which is analyzed
+ *
+ * @return INT, FLOAT64, STRING or IDENTIFIER
+ */
 Datatype getType(Token* token);
+
+/**
+ * @brief Main function for processing expressions
+ *
+ * @param prev_token, input from parser
+ * @param act_token, input from parser
+ *
+ * @return 0, in case of error, number of error
+ */
 int expression(Token* prev_token, Token* act_token);
+
+/**
+ * @brief Function for pushing operands to stack
+ *
+ * @param token, oprand which will be pushed to stack
+ *
+ * @return true, if type was not found false
+ */
 bool cg_stack_p(Token* token);
 
 #endif //IFJ_FINAL_PARSER_H
